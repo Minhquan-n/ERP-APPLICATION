57effbc5079d0a29428fd63e8051c18fc8e341e0
Minhquan-n <mquantruong01@gmail.com>
2023-11-05 00:07:49 +0700

    Chỉnh sửa giao diện trang lỗi 404 - frontend

--- ERPAPP-FRONTEND/src/views/PageNotFound/PageNotFound.vue
+++ ERPAPP-FRONTEND/src/views/PageNotFound/PageNotFound.vue
@@ -1,6 +1,23 @@
 <template>
-    <h1>404 PAGE NOT FOUND!</h1>
-    <router-link :to="{name: 'AdminHomePage'}">
-        Back to Home
-    </router-link>
+    <div id="pageNotFound">
+        <h1>404 PAGE NOT FOUND!</h1>
+        <router-link :to="{name: 'LoginPage'}">
+            Back to Home
+        </router-link>
+    </div>
 </template>
+
+<style>
+    #pageNotFound {
+        width: 100%;
+        height: 500px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    a {
+        font-size: large;
+    }
+</style>
